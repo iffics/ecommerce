cb140cae5ce20d9e5360b20301b022c4677a0778
iffics <iffics777@gmail.com>
2017-03-27 11:47:45 +0500

    Initial commit

---
+++ 2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\john\desktop\p2ve\ecommerce-2\ecommerce-2-master\2\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
